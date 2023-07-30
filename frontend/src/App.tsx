e9f63960a78f90580120bbdf359c9aacb77ca111
Naman Gupta <naman.gupta@harness.io>
2023-07-31 00:04:52 +0530

    feat: add navbar/header/footer

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import {Container} from 'react-bootstrap'
+
+function App() {
+
+  return (
+    <>
+    <Header/>
+    <main className="py-3">
+      <Container>
+
+      <h1>Welcome to ShopEasy</h1>
+      </Container>
+    </main>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
