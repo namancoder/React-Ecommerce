ffb91cd423bdd31fc1fcd9e2b6bdbb998a7f379d
Naman Gupta <naman.gupta@harness.io>
2023-10-07 01:17:54 +0530

    edit name

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <Container>
         <Row>
             <Col className='text-center py-3'>
-                <p>ProShop &copy; {currentYear}</p>
+                <p>ShopEasy &copy; {currentYear}</p>
             </Col>
         </Row>
     </Container>
